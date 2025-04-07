9bc54fc336e2e49cdd8095cde97009b1eef4d6f7
Tremblay Matthieu <matthieu.tremblay@reseau.eseo.fr>
2025-04-07 08:52:53 +0200

    Correction du main h

--- Inc/main.h
+++ Inc/main.h
@@ -59,8 +59,6 @@ void Error_Handler(void);
 /* Private defines -----------------------------------------------------------*/
 #define TRIG_Pin GPIO_PIN_1
 #define TRIG_GPIO_Port GPIOA
-#define LED_GREEN_Pin GPIO_PIN_12
-#define LED_GREEN_GPIO_Port GPIOD
 #define BUTTON_Pin GPIO_PIN_13
 #define BUTTON_GPIO_Port GPIOD
 #define LED_GREEN_Pin GPIO_PIN_12
